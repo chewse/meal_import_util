99254aaa5b5f5d1e8e69cbd965198308b9def310
Solomon Apenya <solawd@gmail.com>
2018-07-30 16:11:14 +0000

    Fix minor typo on MealItem

--- sheets/import_models.py
+++ sheets/import_models.py
@@ -2,7 +2,7 @@
 This module will contain models that will serve as the output items of an import run on the Google sheets
 """
 
-class MealIem:
+class MealItem:
     """Data model for a meal item record"""
     pass
 
